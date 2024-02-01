cbbb1c1d57d7bd41770d42f97057c7ca000ea2ea
Ramis <sir.ramis@mail.ru>
2024-02-01 11:37:59 +0300

    обучаюсь

--- te/admin.py
+++ te/admin.py
@@ -1,16 +1,32 @@
 #from django.contrib import admin
 
-# Register your models here.
+# 1
 a = 4
 if a == 0 or a < 0:
     print(False)
 else:
     print(True)
 
-
+# 2
 a = 1234
 b = a % 2
 if b == 0:
     print('Четное')
 else:
     print('Нечетное')
+
+    #3
+a = 500
+if a % 4 == 0 or a % 400 == 0:
+    print('visokosny')
+
+    #4
+# a = input()
+# b = int(input())
+#
+# print(a * b, end='\n')
+
+    #5
+
+
+
