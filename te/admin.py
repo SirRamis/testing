d2cf3eb29e967344e4f9b7f9192490d2d99b8917
Ramis <sir.ramis@mail.ru>
2024-02-08 11:19:52 +0300

    обучаюсь2

--- te/admin.py
+++ te/admin.py
@@ -1,32 +1,63 @@
-#from django.contrib import admin
-
-# 1
-a = 4
-if a == 0 or a < 0:
-    print(False)
-else:
-    print(True)
-
-# 2
-a = 1234
-b = a % 2
-if b == 0:
-    print('Четное')
-else:
-    print('Нечетное')
-
-    #3
-a = 2004
-if a % 4 == 0 and a % 400 == 0:
-    print('visokosny')
-
+# #from django.contrib import admin
+#
+# # 1
+# a = 4
+# if a == 0 or a < 0:
+#     print(False)
+# else:
+#     print(True)
+#
+# # 2
+# a = 1234
+# b = a % 2
+# if b == 0:
+#     print('Четное')
+# else:
+#     print('Нечетное')
+#
+#     #3
+# a = 400
+# if a % 4 == 0 and a % 100 != 0 or a % 400 == 0:
+#     print('visokosniy')
+# else:
+#     print('nevisokostniy')
+#
     #4
 # a = input()
 # b = int(input())
 #
-# print(a * b, end='\n')
+# for i in range(b):
+#     print(a)
 
-    #5
+     #5
+a = int(input())
+b = int(input())
+op = str(input())
+if op == '+':
+    result = a + b
+elif op == '-':
+    result = a - b
+elif op == '*':
+    result = a * b
+    print(f'{a} {op} {b}')
+elif op == '/':
+    result = a / b
+print(f"{a} {op} {b} = {result}")
 
 
 
+
+
+# i = 0
+# while i < 5:
+#     i += 1
+#     if i == 5:
+#         print(i)
+#
+#
+
+# numbers = [3,4,5,6,7]
+# a = min(numbers)
+# q = numbers.remove(a)
+#
+# print(q)
